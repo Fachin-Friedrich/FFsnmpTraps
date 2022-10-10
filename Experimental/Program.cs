f7be6995909a2d6440715580256aece1e10a6c1e
Marcel Rädeker <m.raedeker@fachin-friedrich.de>
2022-10-10 10:45:00 +0200

    Initial commit

---
+++ Experimental/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Experimental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    } // End of class
+
+} // End of namespace
