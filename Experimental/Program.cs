cebc894b9a3d77475416e93498b7dc1eb8ccb7d3
Marcel Rädeker <m.raedeker@fachin-friedrich.de>
2022-11-08 11:41:19 +0100

    Improved Trap handling

--- Experimental/Program.cs
+++ Experimental/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading;
 using SnmpSharpNet;
 using System.Diagnostics;
+using csJson;
+using System.Net;
 
 namespace Experimental
 {  
@@ -13,67 +15,91 @@ namespace Experimental
         static void WriteLogSimple( string txt)
         {
             Console.WriteLine($"[{DateTime.Now}] {txt}");
-            ELog.WriteEntry(txt);
+            ELog.WriteEntry(
+                message: txt,
+                type: EventLogEntryType.Information,
+                eventID: -1,
+                category: -1
+            );
         }
 
-        static void HandleV1Trap( byte[] raw)
+        static void WriteEvent( IPHostEntry host, SnmpV2Packet pkt, byte[] raw)
         {
+            string hostname = string.IsNullOrEmpty(host.HostName) ? string.Empty : $" ({host.HostName})";
             var output = new System.Text.StringBuilder();
-            var pkt = new SnmpV1TrapPacket();
-            pkt.decode(raw, raw.Length);
 
-            output.AppendLine($"SNMP v1");
-            output.AppendLine($"Generic: {pkt.Pdu.Generic} - Specific: {pkt.Pdu.Specific}");
-            output.AppendLine($"Agent address: {pkt.Pdu.AgentAddress}");
-            output.AppendLine($"Message count: {pkt.Pdu.VbList.Count}");
-            output.AppendLine("---");
-            foreach( var v in pkt.Pdu.VbList)
-            {
-                output.AppendLine($"{v.Oid} - {SnmpConstants.GetTypeName(v.Value.Type)} : {v.Value.ToString()}");
-            }
-
-            string result = output.ToString();
-            WriteLogSimple(result);
-        }
-
-        static void HandleV2Trap( byte[] raw)
-        {
-            var pkt = new SnmpV2Packet();
-            pkt.decode(raw, raw.Length);
-            if( pkt.Pdu.Type != PduType.V2Trap)
-            {
-                throw new Exception("Captured corrupted SNMP V2 Package");
-            }
-
-            var output = new System.Text.StringBuilder();
             output.AppendLine("SNMP v2");
+            output.AppendLine($"Agent address: {host.AddressList[0]}{hostname}");
             output.AppendLine($"Community: {pkt.Community}");
             output.AppendLine($"Message count: {pkt.Pdu.VbList.Count}");
             output.AppendLine("---");
-            foreach( var v in pkt.Pdu.VbList)
+            foreach (var v in pkt.Pdu.VbList)
             {
-                output.AppendLine($"{v.Oid} - {SnmpConstants.GetTypeName(v.Value.Type)} : {v.Value.ToString()}");
+                output.AppendLine($"{v.Oid} - {SnmpConstants.GetTypeName(v.Value.Type)} : {v.Value}");
             }
 
             string result = output.ToString();
-            WriteLogSimple(result);
+            Console.WriteLine($"[{DateTime.Now}] {result}");
+            ELog.WriteEntry(
+                message: result,
+                type: EventLogEntryType.Warning,
+                eventID: pkt.Pdu.ErrorIndex,
+                category: (short) pkt.Pdu.ErrorStatus,
+                rawData: raw
+            );
         }
 
-        static void HandleTrap( byte[] raw)
+        static void WriteEvent( IPHostEntry host, TrapPdu data, byte[] raw)
+        {
+            var output = new System.Text.StringBuilder();
+            string hostname = string.IsNullOrEmpty(host.HostName) ? string.Empty : $" ({host.HostName})";
+
+            output.AppendLine($"SNMP v1");
+            output.AppendLine($"Generic: {data.Generic} - Specific: {data.Specific}");
+            output.AppendLine($"Agent address: {host.AddressList[0]}{hostname}");
+            output.AppendLine($"Message count: {data.VbList.Count}");
+            output.AppendLine("---");
+            foreach (var v in data.VbList)
+            {
+                output.AppendLine($"{v.Oid} - {SnmpConstants.GetTypeName(v.Value.Type)} : {v.Value}");
+            }
+
+            string result = output.ToString();
+            Console.WriteLine($"[{DateTime.Now}] {result}");
+            ELog.WriteEntry(
+                message: result,
+                type: EventLogEntryType.Warning,
+                eventID: data.Specific,
+                category: (short) data.Generic,
+                rawData: raw
+            );
+        }
+
+        static void HandleTrap( byte[] raw, IPEndPoint ep )
         {
             try
             {
                 int version = SnmpPacket.GetProtocolVersion(raw, raw.Length);
+                var hostinfo = System.Net.Dns.GetHostEntry(ep.Address);
                 switch (version)
                 {
                     case (int)SnmpVersion.Ver1:
 
-                        HandleV1Trap(raw);
+                        var pkt1 = new SnmpV1TrapPacket();
+                        pkt1.decode(raw, raw.Length);
+                        WriteEvent(hostinfo, pkt1.TrapPdu, raw);
                         break;
 
                     case (int)SnmpVersion.Ver2:
 
-                        HandleV2Trap(raw);
+                        var pkt2 = new SnmpV2Packet();
+                        pkt2.decode(raw, raw.Length);
+                        if (pkt2.Pdu.Type != PduType.V2Trap)
+                        {
+                            throw new Exception("Captured corrupted SNMP V2 Package");
+                        }
+
+                        WriteEvent(hostinfo, pkt2, raw);
                         break;
 
                     default:
@@ -134,23 +160,30 @@ namespace Experimental
             Console.WriteLine(x.Count);
         }
 
-        static void Main(string[] args)
+        static void Main_dns_check()
         {
-            TestParsing();
-            return;
-            InitEventLog();
+            var z = System.Net.Dns.GetHostEntry("192.168.25.200");
+            Console.WriteLine(z.HostName);
+            for( int i = 0; i < z.Aliases.Length; ++i)
+            {
+                Console.WriteLine($"[{i}] {z.Aliases[i]}");
+            }
+        }
+
+        static void Main_Networkloop(string[] args)
+        {
             var socket = new System.Net.Sockets.UdpClient(162);
 
             while (true)
             {
                 try
                 {
-                    var ep = new System.Net.IPEndPoint(System.Net.IPAddress.Any, 162);
+                    var ep = new IPEndPoint(IPAddress.Any, 162);
                     var buffer = socket.Receive(ref ep);
 
                     if( buffer.Length > 0)
                     {
-                        new Thread(() => HandleTrap(buffer)).Start();
+                        new Thread(() => HandleTrap(buffer, ep)).Start();
                     }
                     else
                     {
